371a35797eb1b510ee680b553224ba126b04d11c
pawarsayali <sayalipawar@ternaengg.ac.in>
2021-04-25 02:41:04 +0530

    completed QR page and displayed map to visitor

--- beproj_app/backend/controllers/visitorMap.js
+++ beproj_app/backend/controllers/visitorMap.js
@@ -1,11 +1,28 @@
 const Map = require("../models/map");
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
 exports.viewMap = function (req, res) {
-    console.log(req.body);
-  Map.findById(req.body.id, function (err, val)  {
-    if (err) throw err;
-    if (val) res.send(val);
-    if (!val) res.status(403).send("Wrong QR code scanned");
-  });
+  // console.log(req.body);
+  const id = req.body.id;
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    // Yes, it's a valid ObjectId, proceed with `findById` call.
+    Map.findById(id, function (err, val) {
+      if (err) throw err;
+      if (val) res.send(val);
+      if (!val) {
+        console.log(!val);
+        res.status(403).send("Wrong QR code scanned");
+      }
+    });
+  } else {
+    res.status(403).send("Wrong QR code scanned");
+  }
 };
+
+// exports.viewMap = async (req, res)=> {
+// const x=await Map.findById(req.body.id);
+// console.log(x);
+// if(x) res.send(x)
+// if(!x)  res.status(403).send("Wrong QR code scanned")
+
+// };
